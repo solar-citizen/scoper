1d3c1245cd0a8e089a3c63a97fb97e57127389d8
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-11-23 01:27:37 +0200

    System: add configuration service

---
+++ src/config/config.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+import { z } from 'zod';
+
+const envs = ['development', 'production'] as const;
+
+const validationObject = z.object({
+  APP_ENV: z.enum(envs),
+  APP_PORT: z.coerce.number().int().min(0).max(65535),
+  REVIEW_INSTRUCTIONS_PATH: z.string(),
+  GITHUB_TOKEN: z.string(),
+  GITHUB_WEBHOOK_SECRET: z.string(),
+  GITHUB_REPO_OWNER: z.string(),
+  GITHUB_REPO_NAME: z.string(),
+  LLM_API_KEY: z.string(),
+  LLM_MODEL: z.string(),
+  LOCAL_LLM_BASE_URL: z.url(),
+  LOCAL_LLM_MODEL: z.string(),
+});
+
+@Injectable()
+export class ConfigService {
+  static validate = (config: Record<string, unknown>): z.infer<typeof validationObject> =>
+    validationObject.parse(config);
+
+  constructor(
+    private readonly configService: NestConfigService<z.infer<typeof validationObject>, true>,
+  ) {}
+
+  get env(): (typeof envs)[number] {
+    return this.configService.get('APP_ENV');
+  }
+
+  get port(): number {
+    return Number(this.configService.get('APP_PORT'));
+  }
+
+  get githubToken(): string {
+    return this.configService.get<string>('GITHUB_TOKEN');
+  }
+
+  get githubWebHookSecret(): string {
+    return this.configService.get<string>('GITHUB_WEBHOOK_SECRET');
+  }
+
+  get githubRepoOwner(): string {
+    return this.configService.get<string>('GITHUB_REPO_OWNER');
+  }
+
+  get githubRepoName(): string {
+    return this.configService.get<string>('GITHUB_REPO_NAME');
+  }
+
+  get llmApiKey(): string {
+    return this.configService.get<string>('LLM_API_KEY');
+  }
+
+  get llmModel(): string {
+    return this.configService.get<string>('LLM_MODEL');
+  }
+
+  get localLlmBaseUrl(): string {
+    return this.configService.get<string>('LOCAL_LLM_BASE_URL');
+  }
+
+  get localLlmModel(): string {
+    return this.configService.get<string>('LOCAL_LLM_MODEL');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const { port } = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
 }
 
 bootstrap().catch((err: unknown) => console.error(err));
