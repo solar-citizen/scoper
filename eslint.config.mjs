482f3e96f4a9f27ed48087a3bef1350b0713c327
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-11-22 02:26:41 +0200

    System: add linting
    + cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,31 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import configPrettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginPromise from 'eslint-plugin-promise';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tsEslint from 'typescript-eslint';
 
 export default defineConfig(
   {
-    ignores: ['eslint.config.mjs', '.prettierrc.mjs', 'bin/*'],
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/@generated/**',
+      '**/eslint.config.mjs',
+    ],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tsEslint.configs.strictTypeChecked,
+  ...tsEslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
       sourceType: 'commonjs',
       parserOptions: {
@@ -24,13 +33,73 @@ export default defineConfig(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
+    plugins: {
+      '@typescript-eslint/eslint-plugin': tsEslint,
+      'simple-import-sort': pluginSimpleImportSort,
+      // @ts-expect-error - plugin has ESM/CJS compatibility issue with flat config types
+      promise: pluginPromise,
+      import: pluginImport,
+      prettier: pluginPrettier,
+    },
     rules: {
+      'max-len': ['warn', { code: 100 }],
+
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'function',
+          format: ['PascalCase', 'camelCase'],
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+        },
+      ],
+      'no-duplicate-imports': 'error',
+      'no-param-reassign': 'error',
+      'object-shorthand': 'error',
+      'require-await': 'error',
+      'no-console': ['warn', { allow: ['error', 'warn'] }],
+      'padding-line-between-statements': [
+        'warn',
+        {
+          blankLine: 'always',
+          prev: ['block', 'block-like', 'multiline-block-like'],
+          next: 'return',
+        },
+        { blankLine: 'always', prev: '*', next: 'if' },
+        { blankLine: 'always', prev: '*', next: 'export' },
+      ],
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'import/max-dependencies': ['warn', { max: 15, ignoreTypeImports: true }],
     },
   },
+  {
+    files: ['**/*.script.ts', '**/prisma/seed.ts'],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.module.ts'],
+    rules: {
+      '@typescript-eslint/no-extraneous-class': 'off',
+    },
+  },
+  configPrettier,
 );
